f7be6f0d535a690846c759b09a45e7df66325a4c
Basruhe <sb.ruhe@gmail.com>
2020-01-30 15:12:24 +0100

    hardcoded product list showing

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,8 +1,51 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default class ProductList extends Component {
-  componentDidMount = () => {};
+// for now: fetch in this component. Later: replace with action
+
+class ProductList extends Component {
+  componentDidMount() {
+    fetch("http://localhost:4000/products")
+      .then(data => data.json())
+      .then(jsonData => {
+        const fetchedData = jsonData;
+        console.log(fetchedData);
+        this.props.dispatch({
+          type: "products/FETCHED",
+          payload: fetchedData
+        });
+      })
+      .catch(console.error);
+  }
+
+  displayProducts = () => {
+    const productList = this.props.data.products;
+    console.log("the productlist", productList);
+    return productList.map(singleProduct => (
+      <div>
+        {singleProduct.name}
+        {singleProduct.price}
+      </div>
+    ));
+  };
+
   render() {
-    return <div>ProductList!</div>;
+    console.log("rendering the products", this.props.data);
+    return (
+      <div>
+        ProductList!
+        {this.displayProducts()}
+      </div>
+    );
   }
 }
+
+function mapStateToProps(reduxState) {
+  return {
+    data: reduxState.items
+  };
+}
+
+const connectingHOC = connect(mapStateToProps);
+const ConnectedProductList = connectingHOC(ProductList);
+export default ConnectedProductList;
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,8 +1,8 @@
 import { combineReducers } from "redux";
-import productsReducer from "../store/products/reducer";
+import items from "../store/products/reducer";
 
 // import
 
 export default combineReducers({
-  products: productsReducer
+  items
 });
